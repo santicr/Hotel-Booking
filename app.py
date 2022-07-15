358dcd8819d2640d9d8eccb35e2111e8f1acff6c
Santiago José <santicr@MacBook-Air-de-Alejandra.local>
2022-07-14 22:59:20 -0500

    Reservas agregadas, datos agregados, las bases de datos son archivos planos, parchivo html error creado para cuando haya error en la reserva de algún hotel, ya se puede reservar todos los hoteles y se agrega correctamente, agregado UUID para poder identificar cada reserva

--- app.py
+++ app.py
@@ -1,24 +1,76 @@
+from audioop import add
 from flask import Flask, request, g, redirect, url_for, render_template, flash, session #Importa las librerías que usaré
+import uuid
 
 app = Flask(__name__)
+nombres_hoteles = ['Intercontinental', 'Ibis', 'Airto', 'Sant']
+
+def confirm_data(datosUsuario):
+    file = open("datos.txt", "r")
+    lineas = file.readlines()
+    flag = True
+
+    for linea in lineas:
+        datos = linea.split()
+        flag = True
+        for i in range(len(datos)):
+            if datos[i] != datosUsuario[i]:
+                flag = False
+        if flag:
+            return True
+
+    file.close()
+    return False
+
+def add_reserva(datosUsuario):
+    f = open("reservas.txt", "a")
+    datos_tam = len(datosUsuario)
+
+    if datos_tam == 1 and datosUsuario[0] != 'tried':
+        f.write(datosUsuario[0] + ' ')
+    elif datos_tam == 1 and datosUsuario[0] == 'tried':
+        f.write('tried\n')
+    else:
+        for i in range(datos_tam):
+            f.write(datosUsuario[i] + ' ')
+        f.write('\n')
+
+    f.close()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/reserva')
-def reserva():
+@app.route('/reserva<int:reserva_id>', methods = ['GET'])
+def reserva(reserva_id):
+    num = int(reserva_id) - 1
+    add_reserva([nombres_hoteles[num]])
     return render_template('reserva.html')
 
 @app.route('/reserva_success', methods = ['GET', 'POST'])
 def reserva_success():
-    correo = request.form['correo']
+    nombre = request.form['nombre']
+    apellidos = request.form['apellidos']
     codSeguridad = request.form['codSeguridad']
     numTarjeta = request.form['numTarjeta']
     fecha = request.form['fecha']
     numPersonas = request.form['numPersonas']
+    path = request.path
+    apellidos = apellidos.split()
+    apellido1, apellido2 = apellidos[0], apellidos[1]
+    datosUsuario = [nombre, apellido1, apellido2, numTarjeta, codSeguridad]
+    datosReserva = [nombre, apellido1, apellido2, fecha, numPersonas, str(uuid.uuid1())]
+
+    if not confirm_data(datosUsuario):
+        add_reserva(['tried'])
+        return redirect(url_for('reserva_error'))
+
+    add_reserva(datosReserva)
 
     return redirect(url_for('index'))
 
+@app.route('/reserva_error')
+def reserva_error():
+    return render_template('error.html')
 
-app.run(debug=True, use_debugger=False, use_reloader=False)
+app.run(debug = True)
